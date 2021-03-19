149ade649c56672b237dfc1c39397e3ab50f958b
Andy <andytcodes@gmail.com>
2021-03-19 18:17:24 -0400

    Install and Import dependencies

---
+++ handpose/src/App.js
@@ -0,0 +1,29 @@
+import Reac, {useRef} from 'react';
+//import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
